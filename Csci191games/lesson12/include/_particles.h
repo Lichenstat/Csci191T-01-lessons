9ac8a654bb508510300c57dd2473926e606134cc
Mvalov <43051717+Mvalov@users.noreply.github.com>
2021-04-14 17:21:55 -0700

    added lesson12

---
+++ Csci191games/lesson12/include/_particles.h
@@ -0,0 +1,33 @@
+#ifndef _PARTICLES_H
+#define _PARTICLES_H
+
+#include <math.h>
+
+#define  50000
+#define GRAVITY -0.001
+
+class _particles
+{
+    public:
+        _particles();
+        virtual ~_particles();
+
+        typedef struct{
+            bool alive;
+            float posX, posY, posZ;
+            float xDir, yDir;
+            float mass;
+        }pts;
+
+        pts drops[MAX_DROPS];
+
+        void drawDrops();
+        void update();
+        void particleGenerator();
+
+    protected:
+
+    private:
+};
+
+#endif // _PARTICLES_H
