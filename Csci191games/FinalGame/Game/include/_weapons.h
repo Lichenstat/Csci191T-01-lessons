c4c182dc41d49ecfb4266d2a84e9b4438cef6147
megafatturtles <79233281+megafatturtles@users.noreply.github.com>
2021-05-09 21:06:32 -0700

    Update _weapons.h

--- Csci191games/FinalGame/Game/include/_weapons.h
+++ Csci191games/FinalGame/Game/include/_weapons.h
@@ -5,35 +5,44 @@
 #include <_object_max.h>
 #include <_objectinteract_max.h>
 #include <_hitbox_max.h>
-#include <_collision.h>
 #include <_texture.h>
 #include <_common.h>
 #include <_timer.h>
 #include <cmath>
+#include <stdlib.h>
+#include <time.h>
 #include <GL/gl.h>
+#include <_sounds.h>
 class _weapons
 {
     public:
         _weapons();
         virtual ~_weapons();
+        void weaponSpawn(_object_max*, _weapons*);
 
-        bool pickUpWeapons(_player*, _weapons*);
-        vec3 weaponPos;
-        //weapons we will be trying to use
-        enum{MELEE, PISTOL, GRENADELAUNCHER, FLAMETHROWER, BEAM, SHOCKRIFLE};
-        void weaponAction();   // This is basically going to be their bullet physics
+        //weapons we will be trying to use -> these are the projectiles
+        enum{PISTOL, GRENADELAUNCHER, BEAM, SHOCKRIFLE};
+        //These are the actual weapons
+        enum{P, G, B, S};
+        void weaponAction(_object_max* ply);   // This is basically going to be their bullet physics
         int action;
+        int weaponHold;
         int weaponDmg;      // This is going to be the amount of damage they can do
 
-        //drawing our projectile and their positions
+        //drawing our projectiles and weapons and their positions
         void drawProj();
+        void drawWeapon();
         _texture *proj = new _texture();
         void projInit(float, float);
         vec3 projPos;
         vec3 projScale;
+        _texture *weaponSkin = new _texture();
+        vec3 weaponPos;
+        vec3 weaponScale;
         vec3 vert[4];
         float xMin, xMax, yMin, yMax;   // number of frames
         float framesX, framesY;   //number of frames
+        bool active;
 
 
 
@@ -41,11 +50,21 @@ class _weapons
         float vel;              //Velocity
         float accel;            //Acceleration
         GLfloat angle;
+        GLfloat rotateAngle;
         float xDisplace();      //Delta X
         float yDisplace();      //Delta Y
         float tick;             //time in ticks
-        _collision *col = new _collision();     //checking collisions with enemies and walls.
+        float weaponFall();
+        int onlyOnceP;
+        int onlyOnceG;
+        int onlyOnceB;
+        int onlyOnceS;
+        void toOrigin(_object_max*);
+        void hitToOrigin(_object_max*);
+        float xClicker;
+        float yClicker;
 
+        _sounds* snds = new _sounds();
     protected:
 
     private:
