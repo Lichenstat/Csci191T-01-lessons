d16ca10c762311ba6312b15f601028d2ad18db4f
Mvalov <43051717+Mvalov@users.noreply.github.com>
2021-05-12 15:45:42 -0700

    push to main

--- Csci191games/FinalGame/Game/src/_glScene.cpp
+++ Csci191games/FinalGame/Game/src/_glScene.cpp
@@ -1497,11 +1497,8 @@ int _glScene::winMSG(HWND   hWnd,			        // Handle For This Window
 
     case WM_MOUSEMOVE:
     {
-<<<<<<< HEAD
         kbMS->anglesForShots(wpns, LOWORD(lParam), HIWORD(lParam));
-=======
         kbMS->mouseMove(modelTeapot, LOWORD(lParam), HIWORD(lParam));
->>>>>>> 8597067834ccc8a6194c7bd64862d57fdaf6eb1a
         break;
     }
 
